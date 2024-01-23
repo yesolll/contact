3ce131db4bd6351c3365a7c70aaddde9ba3fae64
yesolll <9595kys@gmail.com>
2024-01-23 14:49:33 +0900

    5. Task: Layout & Styling

--- lib/main.dart
+++ lib/main.dart
@@ -20,27 +20,45 @@ class MyApp extends StatelessWidget {
             // ...
           ],
         ),
-        body: SizedBox(
+        body: Container(
+          margin: EdgeInsets.all(10),
+          height: 100,
           child: Row(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              ElevatedButton(
-                onPressed: (){},
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.black),
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex:3,
+                child: Container(
+                  height: 100,
+                  padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                  child: Image(
+                      fit: BoxFit.fitHeight,
+                      image: AssetImage('assets/images/example_img.jpg')
+                  ),
+                )
+              ),
+              Expanded(
+                flex: 7,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('코치맨 레이저 675 중고 카라반 판매'),
+                    Text(
+                      style: TextStyle(fontSize: 11, color: Colors.black45),
+                        '영통구 이의동 · 끌올 10분 전'
+                    ),
+                    Text('40,000,000원'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Icon(Icons.favorite),
+                        Text('13'),
+                      ],
+                    ),
+                  ],
                 ),
-                child: const Text('ElevatedButton'),
-              ), TextButton(
-                child: const Text('TextButton'),
-                onPressed: () {
-                  // Respond to button press
-                },
-              ), IconButton(
-                  onPressed: () {
-                    // Respond to button press
-                  }, icon: const Icon(Icons.accessibility),
-              )
+              ),
             ],
           ),
           // child: Text(
